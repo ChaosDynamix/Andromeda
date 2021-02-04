e077b7b1dbfb3dcfd5e7f4cca4d05a17e1eb5b44
Chaosdynamix <chaosdynamix@posteo.net>
2021-02-04 23:08:51 +0100

    fix(gatsby): clean index & integrate header (#17)
    Clean the index page by removing maintenance section.
    Add Header component.
    Add blank section.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,17 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function Home() {
-  return (
-    <div className="h-screen bg-blue-900">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span className="block text-blue-50">Andromeda</span>
-          <span className="block text-blue-300">Website under maintenance.</span>
-        </h2>
-        <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-          <div className="inline-flex rounded-md shadow">
-            <a href="https://github.com/ChaosDynamix/Andromeda" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-500">
-              GitHub repository
-            </a>
-          </div>
+    return (
+        <div className="h-screen bg-blue-900">
+
+            {/* Header */}
+            <Header />
+
+            {/* Page */}
+            <section>
+            </section>
+
         </div>
-      </div>
-    </div>
-  );
+    );
 }
